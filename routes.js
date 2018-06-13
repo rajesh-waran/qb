a590f43dd1df323bd3c29bb21d7e49260e135913
Rajeshwaran <rajeshfederer2@gmail.com>
2018-06-13 14:57:31 +0530

    changes based on comments

--- routes.js
+++ routes.js
@@ -25,22 +25,7 @@ router.post('/dialogflowAPI', function (req, res) {
 		if (error) {
 			res.json({ error: "error in chat server api call" }).end();
 		} else {
-			
-			if(body.result && body.result.action && body.result.action=='discount.discount-custom'){
-
-				let resolvedQuery = body.result.resolvedQuery;
-				let count = resolvedQuery.split(',').length;
-
-				if(count <4){
-					body.result.fulfillment.messages[0].speech = body.result.fulfillment.messages[0].speech.replace('2  coverage', count+' coverage')
-					body.result.fulfillment.messages[2].speech = '<a class="pdfClass" data-toggle="modal" data-target="#fundModal">Click here</a> to refer the discount chart for more details'
-				} else {
-					body.result.fulfillment.messages[1].speech = "We already offering an exciting <b>20% discount</b> for a minimum of <b>4 coverage</b> sections";
-					body.result.fulfillment.messages.splice(0,1);
-					body.result.fulfillment.messages[1].speech = '<a class="pdfClass" data-toggle="modal" data-target="#fundModal">Click here</a> to refer the discount chart for more details'
-				}
-				res.json(body).end();
-			} else if(body.result && body.result.action && body.result.action =="riskClass"){
+			if(body.result && body.result.action && body.result.action =="riskClass"){
 				
 				let riskClass = body.result.parameters && body.result.parameters.RiskClass ? body.result.parameters.RiskClass : null;
 				console.log('YEAH', riskClass, body.result.parameters);
